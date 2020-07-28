9f9bdcf9753c53ed25e51f4f26d5791e465c3297
luo0412 <1095847440@qq.com>
2020-07-29 00:41:32 +0800

    small

--- common/src/main/java/com/example/common/core/domain/BaseEntity.java
+++ common/src/main/java/com/example/common/core/domain/BaseEntity.java
@@ -23,20 +23,25 @@ public class BaseEntity implements Serializable
     private String searchValue;
 
     /** 创建者 */
+    @TableField(exist = false)
     private String createBy;
 
     /** 创建时间 */
+    @TableField(exist = false)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
     /** 更新者 */
+    @TableField(exist = false)
     private String updateBy;
 
     /** 更新时间 */
+    @TableField(exist = false)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
     /** 备注 */
+    @TableField(exist = false)
     private String remark;
 
     /** 开始时间 */
