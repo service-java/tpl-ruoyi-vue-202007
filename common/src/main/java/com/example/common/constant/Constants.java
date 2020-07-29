8a6a8ffc50d3d2929e0c2985be7f0e42789b9132
luo0412 <1095847440@qq.com>
2020-07-29 17:11:27 +0800

    [结构](small)

--- common/src/main/java/com/example/common/constant/Constants.java
+++ common/src/main/java/com/example/common/constant/Constants.java
@@ -128,11 +128,11 @@ public class Constants
     /**
      * 当前页码
      */
-    public static final String PAGE = "page";
+    public static final String PAGE = "pageNum";
     /**
      * 每页显示记录数
      */
-    public static final String LIMIT = "limit";
+    public static final String LIMIT = "pageSize";
     /**
      * 排序字段
      */
@@ -145,91 +145,6 @@ public class Constants
      *  升序
      */
     public static final String ASC = "asc";
-    /**
-     * 菜单类型
-     *
-     * @author chenshun
-     * @email sunlightcs@gmail.com
-     * @date 2016年11月15日 下午1:24:29
-     */
-    public enum MenuType {
-        /**
-         * 目录
-         */
-        CATALOG(0),
-        /**
-         * 菜单
-         */
-        MENU(1),
-        /**
-         * 按钮
-         */
-        BUTTON(2);
 
-        private int value;
 
-        MenuType(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
-    }
-
-    /**
-     * 定时任务状态
-     *
-     * @author chenshun
-     * @email sunlightcs@gmail.com
-     * @date 2016年12月3日 上午12:07:22
-     */
-    public enum ScheduleStatus {
-        /**
-         * 正常
-         */
-        NORMAL(0),
-        /**
-         * 暂停
-         */
-        PAUSE(1);
-
-        private int value;
-
-        ScheduleStatus(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
-    }
-
-    /**
-     * 云服务商
-     */
-    public enum CloudService {
-        /**
-         * 七牛云
-         */
-        QINIU(1),
-        /**
-         * 阿里云
-         */
-        ALIYUN(2),
-        /**
-         * 腾讯云
-         */
-        QCLOUD(3);
-
-        private int value;
-
-        CloudService(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
-    }
 }
